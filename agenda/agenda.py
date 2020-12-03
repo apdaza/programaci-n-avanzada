5b1415cb51442698d6712f2479cd69546e304996
apdaza <apdaza@gmail.com>
2020-12-03 14:41:18 -0500

    agregado ejemplo de agenda en mysql y flask

---
+++ agenda/agenda.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flaskext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'helloworld'
+app.config['MYSQL_DATABASE_DB'] = 'agenda'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 6603
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor =conn.cursor()
+
+@app.route('/usuarios')
+def ver_usuarios():
+    cursor.execute("SELECT * from usuarios")
+    data = cursor.fetchall()
+    return render_template('usuarios.html', usuarios = data )
+
+@app.route('/todos')
+def ver_todos():
+    cursor.execute("SELECT usu_nombre, con_nombre, con_apellido, con_telefono, cit_lugar, cit_fecha " +
+                    "FROM usuarios as usu " + 
+                    "LEFT JOIN contactos as con on (usu.usu_id = con.usu_id) " +
+                    "LEFT JOIN citas as cit on (con.con_id = cit.con_id)")
+    data = cursor.fetchall()
+    return render_template('usuarios.html', usuarios = data )
+
+if __name__ == '__main__':
+    app.run(debug=True)
