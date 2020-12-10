5d60bdec103e18f62f86fb2ef0dcdeafcf4c823b
apdaza <apdaza@gmail.com>
2020-12-10 15:28:20 -0500

    Ageragdos ejemplos de editar, eliminar y agregar data

--- agenda/agenda.py
+++ agenda/agenda.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -19,6 +19,23 @@ def ver_usuarios():
     data = cursor.fetchall()
     return render_template('usuarios.html', usuarios = data )
 
+@app.route('/contactos')
+def ver_contactos():
+    cursor.execute("SELECT usu_nombre, con_nombre, con_apellido, con_direccion, con_telefono, con_email " +
+                    "FROM usuarios as usu " + 
+                    "INNER JOIN contactos as con on (usu.usu_id = con.usu_id) ")
+    data = cursor.fetchall()
+    return render_template('contactos.html', contactos = data )
+
+@app.route('/citas')
+def ver_citas():
+    cursor.execute("SELECT usu_nombre, con_nombre, con_apellido, con_telefono, cit_lugar, cit_fecha, cit_hora, cit_descripcion " +
+                    "FROM usuarios as usu " + 
+                    "LEFT JOIN contactos as con on (usu.usu_id = con.usu_id) " +
+                    "LEFT JOIN citas as cit on (con.con_id = cit.con_id)")
+    data = cursor.fetchall()
+    return render_template('citas.html', citas = data )
+
 @app.route('/todos')
 def ver_todos():
     cursor.execute("SELECT usu_nombre, con_nombre, con_apellido, con_telefono, cit_lugar, cit_fecha " +
@@ -27,6 +44,42 @@ def ver_todos():
                     "LEFT JOIN citas as cit on (con.con_id = cit.con_id)")
     data = cursor.fetchall()
     return render_template('todos.html', citas = data )
+
+@app.route("/agregar_usuario", methods = ['GET', 'POST'])
+def agregar_usuario():
+    if request.method == 'POST':
+        nombre = request.form["nombre"]
+        clave = request.form["clave"]
+        cursor.execute("""INSERT INTO `usuarios`(`usu_nombre`, `usu_clave`) VALUES (%s, sha1(%s));""", (nombre, clave)) 
+        conn.commit()
+        return redirect(url_for('ver_usuarios'))
+    else:
+        return render_template('agregar_usuario.html')
+
+@app.route('/actualizar_usuario', methods=['GET', 'POST'])
+def actualizar_usuario():
+    if request.method == 'POST':
+        id = request.form["id"]
+        nombre = request.form["nombre"]
+        clave = request.form["clave"]
+        cursor.execute("""UPDATE `usuarios` SET `usu_nombre`=%s,`usu_clave`=sha1(%s) WHERE `usu_id`= %s""", (nombre, clave, id)) 
+        conn.commit()
+        return redirect(url_for('ver_usuarios'))
+    else:
+        id = request.args["id"]
+        cursor.execute("SELECT usu_id, usu_nombre from usuarios where usu_id = " + id)
+        usuario = cursor.fetchone()
+        return render_template('editar_usuario.html', usuario=usuario)
+        
+
+@app.route('/eliminar_usuario', methods=['GET'])
+def eliminar_usuario():
+    if request.method == 'GET':
+        id = request.args["id"]
+        cursor.execute("DELETE from usuarios where usu_id = " + id)
+        conn.commit()
+        return redirect(url_for('ver_usuarios'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
